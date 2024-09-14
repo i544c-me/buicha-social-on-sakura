51c407ee9ad3ae3a05b7182f29d3d58296d17e1a
i544c <vrc@i544c.me>
2024-09-15 03:13:59 +0900

    feat: add k3s-server

---
+++ terraform/compute.tf
@@ -0,0 +1,26 @@
+resource "sakuracloud_server" "k3s_server" {
+  name   = "k3s-server"
+  core   = 1
+  memory = 1
+  disks  = [sakuracloud_disk.k3s_server.id]
+
+  disk_edit_parameter {
+    hostname = "k3s-server"
+    ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2Rn7JefBOiZlbRfEdgEHYneUT8Bl1LH7I08cC2lxWP"]
+  }
+
+  network_interface {
+    upstream = "shared"
+  }
+
+  tags = ["buichasocial"]
+}
+
+data "sakuracloud_archive" "ubuntu" {
+  os_type = "ubuntu2204"
+}
+
+resource "sakuracloud_disk" "k3s_server" {
+  name              = "k3s-server"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+}
---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "k3s_server_ip" {
+  value = sakuracloud_server.k3s_server.ip_address
+}
